bb25260a8a2db73739c954614478811921b55d98
Franco Siccatto <fsiccatto@gmail.com>
2022-06-18 01:05:31 -0300

    Reto 16 completado!

--- 16-descifrandoNumeros.js
+++ 16-descifrandoNumeros.js
@@ -8,8 +8,6 @@
 
 Los símbolos se restan si están inmediatamente a la izquierda de otro mayor. */
 function decodeNumbers(symbols) {
-  let sum = 0;
-  let lastSymbol = 0;
   const symbolsValues = {
     ".": 1,
     ",": 5,
@@ -17,9 +15,7 @@ function decodeNumbers(symbols) {
     ";": 50,
     "!": 100,
   };
-
   symbols = symbols.split("").reverse();
-  symbols;
   return symbols.reduce((sum, symbol, i) => {
     if (!symbolsValues[symbol]) return NaN;
     return symbolsValues[symbols[i - 1]] > symbolsValues[symbol]
